0c515721272a6cbca820fc470232c80a5c11a681
titusmaxentius <rwarner@dalkewarnerdesigns.com>
2021-12-22 12:09:29 -0800

    Ribbed Hat test in knitout (init commit)

---
+++ knitoutTests/RibbedHat.js
@@ -0,0 +1,15 @@
+// Ribbed Hat: Written in KnitoutJS 
+// Testing using: 
+//  Interlocked waste yarn cast on 
+//  One color 2x2 rib with full fashioned decreases
+
+// Initially Created: 22 Dec 2021
+
+const knitout = require('knitout');
+k = new knitout.Writer({carriers: [
+    '1', '2', '3'
+]});
+
+k.addHeader('Machine', 'Kniterate');
+k.addHeader('Gauge', '7');
+
